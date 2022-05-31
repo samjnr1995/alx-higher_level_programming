9c0118cb38da42f39df821397f4e15dbea9981ff
root <suduu1995@gmail.com>
2022-05-31 03:52:51 -0700

    random numbers

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(-20, 20)
+if number > 0:
+print("{:d} is positive".format(number))
+elif number == 0:
+print("{:d} is zero".format(number))
+else:
+print("{:d} is negative".format(number))
